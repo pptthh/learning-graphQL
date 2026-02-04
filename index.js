4ee78ecbe22b6952bdb8d95f731a009fb7c8a556
Tóth Péter <pptthh@gmail.com>
2026-02-04 12:25:34 +0100

    finished initial setup

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const PORT = 8080;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('GraphQL is amazing!');
+    res.send(
+        '<a href="/graphql">GraphQL</a> is amazing!<br/><br/>'+
+        'Test Hello World query by clickng <a href="/graphql?query=query%7Bhello%7D">here!</a>'
+    );
 });
 
-const root = { hello: () => "Hi, I'm Manny!"};
+const root = { hello: () => "Hi, I'm Péter's first GraphQL server response!"};
 
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
+    schema,
     rootValue: root,
     graphiql: true,
 }));
