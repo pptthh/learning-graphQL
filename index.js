50b7bf6f523184fdca5bb553c2d4dfcf3476c84e
Tóth Péter <pptthh@gmail.com>
2026-02-04 11:11:04 +0100

    video-01-04

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './data/schema';
 
 const PORT = 8080;
 
@@ -7,5 +9,13 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
+
+const root = { hello: () => "Hi, I'm Manny!"};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(PORT, () => console.log(`Running server on localhost:${PORT}/graphql`));
